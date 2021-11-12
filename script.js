b59178fd978c8b68b9185e141a13f7ce590e43a9
prabhawagrawal <77531005+prabhawagrawal@users.noreply.github.com>
2021-11-12 11:25:49 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function addUser(){
-    player1_Name = document.getElementById("player-1").value;
-    player2_Name = document.getElementById("player-2").value;
-
-    localStorage.setItem("Player1-Name", player1_Name);
-    localStorage.setItem("Player2-Name", player2_Name);
-
-    window.location.replace("game_page.html");
-}
